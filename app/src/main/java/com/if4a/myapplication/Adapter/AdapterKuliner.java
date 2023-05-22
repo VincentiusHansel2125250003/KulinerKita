8b9a51ea56fa9fc31eb27241cf4bc7406fe568ea
VincentiusHansel2125250003 <hansel.irsansaputra@mhs.mdp.ac.id>
2023-05-22 09:35:24 +0700

    Update 22/05/2023

--- app/src/main/java/com/if4a/myapplication/Adapter/AdapterKuliner.java
+++ app/src/main/java/com/if4a/myapplication/Adapter/AdapterKuliner.java
@@ -1,20 +1,31 @@
 package com.if4a.myapplication.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.if4a.myapplication.API.APIRequestData;
+import com.if4a.myapplication.API.RetroServer;
+import com.if4a.myapplication.Activity.MainActivity;
 import com.if4a.myapplication.Model.ModelKuliner;
+import com.if4a.myapplication.Model.ModelResponse;
 import com.if4a.myapplication.R;
 
 import java.util.List;
 
-public class AdapterKuliner extends RecyclerView.Adapter<AdapterKuliner.VHKuliner>{
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AdapterKuliner extends RecyclerView.Adapter<AdapterKuliner.VHKuliner> {
 
     private Context ctx;
     private List<ModelKuliner> listKuliner;
@@ -27,7 +38,7 @@ public class AdapterKuliner extends RecyclerView.Adapter<AdapterKuliner.VHKuline
     @NonNull
     @Override
     public VHKuliner onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View varView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_kuliner, parent, false);
+        View varView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_kuliner, parent, false);
         return new VHKuliner(varView);
     }
 
@@ -45,7 +56,7 @@ public class AdapterKuliner extends RecyclerView.Adapter<AdapterKuliner.VHKuline
         return listKuliner.size();
     }
 
-    public class VHKuliner extends RecyclerView.ViewHolder{
+    public class VHKuliner extends RecyclerView.ViewHolder {
 
         TextView tvID, tvNama, tvAsal, tvDeskripsiSingkat;
 
@@ -57,6 +68,54 @@ public class AdapterKuliner extends RecyclerView.Adapter<AdapterKuliner.VHKuline
             tvAsal = itemView.findViewById(R.id.tv_asal);
             tvDeskripsiSingkat = itemView.findViewById(R.id.tv_deskripsi);
 
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder pesan = new AlertDialog.Builder(ctx);
+                    pesan.setTitle("Perhatian");
+                    pesan.setMessage("Operasi apa yang akan dilakukan?");
+                    pesan.setCancelable(true);
+
+                    pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            hapusKuliner(tvID.getText().toString());
+                            dialog.dismiss();
+                        }
+                    });
+
+                    pesan.setPositiveButton("Ubah", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+                    pesan.show();
+                    return false;
+                }
+            });
+        }
+
+        private void hapusKuliner(String idKuliner) {
+            APIRequestData ard = RetroServer.konekRetrofit().create(APIRequestData.class);
+            Call<ModelResponse> proses = ard.ardDelete(idKuliner);
+
+            proses.enqueue(new Callback<ModelResponse>() {
+                @Override
+                public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                    String kode = response.body().getKode();
+                    String pesan = response.body().getPesan();
+
+                    Toast.makeText(ctx, "Kode: " + kode + ", Pesan: " + pesan, Toast.LENGTH_SHORT).show();
+                    ((MainActivity) ctx).retrieveKuliner();
+                }
+
+                @Override
+                public void onFailure(Call<ModelResponse> call, Throwable t) {
+                    Toast.makeText(ctx, "Gagal Menghubungi Server!", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
